c59eb1d653b3fd9d5550eb08f535508ab8167e17
Toan Dinh <taplamtiktok686@gmail.com>
2024-12-31 09:45:01 +0700

    add Bai6

---
+++ Bai6/main.js
@@ -0,0 +1,28 @@
+/**
+ * Comment trong JS
+ * 
+ * 1.Mục đích sử dụng
+ * -Ghi chú
+ * -Vô hiệu hóa mã 
+ * 
+ * 2.Loại comment
+ * - Comment 1 dòng
+ * - Comment nhiều dòng 
+ * 
+ * 3.Sử dụng phím tắt
+ * Windows: Ctrl + /
+ * MacOS : Command + /
+ */
+/*
+/**
+ * Khai bao bien
+ * Khai bao ...
+ */
+
+var FullName 'Dinh Xuan Toan'
+var age '24' // Khai bao tuoi
+//Gọi hàm thông báo 
+/*
+alert(FullName);
+alert(age);
+*/
