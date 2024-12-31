ef7a705d753f9c8fff8c64d96109f4382819b769
Toan Dinh <taplamtiktok686@gmail.com>
2024-12-31 10:55:22 +0700

    add Bai8

---
+++ Bai8/main.js
@@ -0,0 +1,15 @@
+/**
+ * Giới thiệu về toán tử trong Javascript 
+ * 1.Toán tử số học - ArithmeticArithmetic
+ * 2.Toán tử gán - Assignment
+ * 3.Toán tử so sánh - ComparisonComparison
+ * 4. Toán tử logic - logical
+ */
+
+var a = 1;
+var b = 2;
+
+if (a > 0 && b > 0) {
+alert ('a & b lớn hơn 0')
+
+}
