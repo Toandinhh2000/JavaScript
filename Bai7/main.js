6f44affb528957dc39524168b38a5680a0602f00
Toan Dinh <taplamtiktok686@gmail.com>
2024-12-31 10:20:10 +0700

    add Bai7

---
+++ Bai7/main.js
@@ -0,0 +1,16 @@
+/*
+ giới thiệu một số hàn built-in 
+ 1.alert
+ 2.console
+ 3.confirm
+ 4.prompt
+ 5.set timeout
+ 8.set interval
+
+ */
+
+setInterval (function() {
+
+console.log ('Day la log'+ Math.random())
+} , 1000)
+
