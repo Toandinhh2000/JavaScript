95c4e10d92fecedb2375d441ae54dbde4efb2cdd
Toan Dinh <taplamtiktok686@gmail.com>
2025-01-01 14:24:53 +0700

    add Bai9

---
+++ Bai9/main.js
@@ -0,0 +1,15 @@
+/** Toán tử số học
+ +        --> Cộng
+ -        --> Trừ
+ *        --> Nhân
+ **       --> Lũy thừa
+ /        --> Chia
+ %        --> Chia lấy số dư
+ ++       --> Tăng một giá trị số
+ --       --> Giảm một giá trị số
+ */
+
+var a = 5;
+
+a--;
+console.log(a)
