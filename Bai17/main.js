6b15557812d069dc8587e7e594897ff972b184be
Toan Dinh <taplamtiktok686@gmail.com>
2025-01-02 10:41:56 +0700

    add Bai17

---
+++ Bai17/main.js
@@ -0,0 +1,59 @@
+
+
+
+// Number typetype
+var a = 1;
+var b = 2;
+var c = 1.5;
+
+console.log(typeof a)
+
+// String type
+var FullName = 'Toan Dinh';
+
+console.log(FullName);
+
+//Boolean typetype
+var isSuccess = true;
+
+//Undefined type
+var age;
+
+console.log(age)
+
+//Null typetype
+var isNull = null; // nothing
+
+console.log(isNull)
+
+//Symbol
+var id = Symbol('id'); // Unique
+var id2 = Symbol('id'), // Unique 
+
+//Function
+
+    var myFunction = Function () {
+            alert('Hi. Xin chao cac ban!');
+    }
+
+    //Object type 
+
+
+    var myObject = {
+        Name: 'Toan Dinh',
+        Age: 24,
+        adress: 'Ninh Binh'
+        myFunction: Function() {
+
+        console.log('myObject', myObject);
+
+        }
+    };
+
+    var myArray = [
+        'Javascript',
+        'PHP',
+        'Ruby',
+
+        console.log(myArray)
+    ];
