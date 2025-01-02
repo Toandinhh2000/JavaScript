b5d57b7a8abc0f63a155dca4ddb2d4907e40302a
Toan Dinh <taplamtiktok686@gmail.com>
2025-01-02 09:06:07 +0700

    add Bai13

---
+++ Bai13/main.js
@@ -0,0 +1,21 @@
+/** Toán tử so sánhsánh
+ 
+Toán tử
+==      -->   bằng
+!=      -->   không bằng 
+>       --> lớn hơn 
+<       --> nhỏ hơn 
+<=      --> nhỏ hơn hoặc bằng 
+>=      --> lớn hơn hoặc bằng 
+
+
+ */
+
+var a = 1;
+var b = 2;
+
+if (a != b ) {
+console.log('Dieu kien dung!');
+} else {
+console.log('Dieu kien sai!');
+}
