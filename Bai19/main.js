f3c42f70aac427ec5bdb692971c8bccd02cdabe0
Toan Dinh <taplamtiktok686@gmail.com>
2025-01-03 08:21:59 +0700

    add Bai19

---
+++ Bai19/main.js
@@ -0,0 +1,20 @@
+
+
+
+
+var a =1;
+var b =2;
+
+var result = a < b && a > 0
+
+console.log('result', result )
+
+if (a < b) {
+    console.log(' A < B');
+} else {
+    console.log('A > B'); 
+}
+
+
+
+
