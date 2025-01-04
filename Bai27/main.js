c6b0d88ccae164351954955c008ff7fb281a8bdd
Toan Dinh <taplamtiktok686@gmail.com>
2025-01-04 19:40:57 +0700

    add Bai27

---
+++ Bai27/main.js
@@ -0,0 +1,17 @@
+
+
+
+
+var isConfirm = confirm('Message?');
+
+console.log(isConfirm);
+
+function cong(a, b) {
+    return a.toString()+ b.toString();
+     console.log('123')
+     alert()
+}
+
+var result = cong(2, 8);
+
+console.log(result)
