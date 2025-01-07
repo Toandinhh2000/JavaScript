a083e838b6a521f583c1bd3a43d04f71135b46a5
Toan Dinh <taplamtiktok686@gmail.com>
2025-01-07 10:29:06 +0700

    add Bai41

---
+++ Bai41/main.js
@@ -0,0 +1,18 @@
+
+var myArray = [
+'Javascript',
+'PHP',
+'Java',
+'Dart',
+'Ruby',
+'python'
+];
+
+console.log(myArray[0]);
+
+var arrayLength = myArray.length;
+
+for (var i = 0; i< 4; i++) {
+    console.log(myArray[i]);
+}
+
