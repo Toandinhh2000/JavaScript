dfe94d56731f8ff5beb872f96bd572ff2bd25f31
Toan Dinh <taplamtiktok686@gmail.com>
2025-01-07 09:13:49 +0700

    add Bai36

---
+++ Bai36/main.js
@@ -0,0 +1,19 @@
+
+
+
+
+var date = 2;
+
+switch(date) {
+    case 2:
+        console.log('Hôm nay là thứ 2');
+        break;
+     case 3:
+        console.log('Hôm nay là thứ 2');
+        break;
+     case 4:
+        console.log('Hôm nay là thứ 2');
+        break;
+}
+
+
