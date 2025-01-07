e758cc5c880adbd89d1466eab2a3d9e60c3769e9
Toan Dinh <taplamtiktok686@gmail.com>
2025-01-07 20:13:29 +0700

    add Bai45

---
+++ Bai45/main.js
@@ -0,0 +1,19 @@
+
+
+
+var i = 0;
+var isSuccess = false;
+
+
+
+do {
+    i++;
+
+    console.log('Nạp thẻ lần' + i);
+    if (false) {
+        isSuccess = true;
+    }
+} while(!isSuccess && i <= 3 );
+
+
+
