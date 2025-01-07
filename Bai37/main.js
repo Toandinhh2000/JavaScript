06f9a00b3b7c20e3c8d7c65f50028a476ba6316c
Toan Dinh <taplamtiktok686@gmail.com>
2025-01-07 09:36:09 +0700

    add Bai37

---
+++ Bai37/main.js
@@ -0,0 +1,15 @@
+
+
+
+var course = {
+    name: 'Javascript'
+    coin: 250
+}
+
+
+var result = course.coin > 0 ? `${course.coin} coins` : 'Miễn phí'
+
+var a = 1;
+var b = 2;
+
+var c = a > 0 ? a: b;
