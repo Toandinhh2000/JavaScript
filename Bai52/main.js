1a2e7ad24d2050806b71e1e84ac9b66e121b719c
Toan Dinh <taplamtiktok686@gmail.com>
2025-01-08 20:32:46 +0700

    add Bai52

---
+++ Bai52/main.js
@@ -0,0 +1,41 @@
+
+
+
+var courses = [
+    {
+        id: 1,
+        name:'Javascript',
+        coin:100
+    }
+    {
+        id: 2,
+        name:'HTML, CSS',
+        coin:200
+    };
+    {
+        id:3,
+        name: 'ruby',
+        coin:220
+    };
+    {
+        id:4,
+        name: 'PHP'
+        coin:200
+    };
+    {
+        id:5,
+        name:'ReactJS'
+        coin:480
+    };
+];
+
+var totalCoin = courses.reduce(function(total, course)
+ i++;
+console.log(i, total, course);
+
+return total + course.coin;
+);
+
+
+
+
