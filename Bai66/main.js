6e1e5180d28620e22930cb07a0ba0af88812b96f
Toan Dinh <taplamtiktok686@gmail.com>
2025-01-13 10:55:05 +0700

    add Bai66

---
+++ Bai66/main.js
@@ -0,0 +1,17 @@
+// HTML DOM
+
+ // 1. Element: ID, class, tag, 
+ // CSS selector, HTML selection
+ // 2. Attribute
+ // 3. Text
+
+var headingNode = document.getElementById('heading');
+
+console.log({
+    Element: headingNode
+});
+
+
+
+
+
