5bd2d7e50ed988f0f30e11e89736553ba012286b
Toan Dinh <taplamtiktok686@gmail.com>
2025-01-14 10:19:06 +0700

    add Bai76

---
+++ Bai76/main.js
@@ -0,0 +1,15 @@
+
+
+
+
+var elenment = 
+document.querySelectorAll('h1');
+
+for(var i =0; i < h1Element.length; ++i) {
+    h1Element[i].onclick = function(e) {
+        console.log(e.target);
+    }
+    
+}
+
+
