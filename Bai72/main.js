d90f18bdc772bbac5bc5795e4a2fb0f17d1b66f2
Toan Dinh <taplamtiktok686@gmail.com>
2025-01-14 08:38:05 +0700

    add Bai72

---
+++ Bai72/main.js
@@ -0,0 +1,12 @@
+
+// HTML DOM
+
+ // 1. Element
+ // 2. Attribute
+ // 3. Text
+
+var boxElement = document.querySelector('.box');
+
+boxElement.outerHTML = '<span>Test</span>';
+
+console.log(boxElement);
