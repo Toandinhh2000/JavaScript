8dd2a3c2142d7b58499a73e46efbf13e625a02c7
Toan Dinh <taplamtiktok686@gmail.com>
2025-01-15 14:47:14 +0700

    add Bai78

---
+++ Bai78/main.js
@@ -0,0 +1,17 @@
+
+
+
+document.querySelector('div').onclick =
+function() {
+    console.log('DIV')
+}
+
+
+document.querySelector('buttom').onclick =
+function(e) {
+    console.log('Click me ')
+}
+
+
+
+
