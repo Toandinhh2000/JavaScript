73263fabc49329abf05d037832db1c0ba08ef04d
Toan Dinh <taplamtiktok686@gmail.com>
2025-01-16 16:09:31 +0700

    add 'Bai90  Promise trong Javascript'

---
+++ Bai90  Promise trong Javascript/app.js
@@ -0,0 +1,16 @@
+
+
+
+
+var promise = new Promise(
+	//Executor
+	function(resolve, reject) {
+	// Logic
+	// Thành công: resolve()
+	// Thất bại:reject()
+
+	//Fake call API
+
+reject('Co loi!');
+	}
+);
