31828bbe4ceb336c505c475ca3e4b8b8f15f8d09
Toan Dinh <taplamtiktok686@gmail.com>
2025-01-17 09:01:11 +0700

    add 'Bai97 Classes trong JavaScript ES6'

---
+++ Bai97 Classes trong JavaScript ES6/app.js
@@ -0,0 +1,17 @@
+
+
+
+function Course(name, price) {
+  this.name = name;
+  this.price = price;
+}
+
+const phpCourse = new Course('PHP', 1000);
+const jsCourse = new Course('Javascript', 1200);
+
+console.log(phpCourse);
+console.log(jsCourse);
+
+
+
+
