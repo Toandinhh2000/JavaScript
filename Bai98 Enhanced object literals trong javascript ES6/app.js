ee03a8d72c1e5b86efb0a052dd4c5d0f7c1f0535
Toan Dinh <taplamtiktok686@gmail.com>
2025-01-17 09:18:31 +0700

    add'Bai98 Enhanced object literals trong javascript ES6'

---
+++ Bai98 Enhanced object literals trong javascript ES6/app.js
@@ -0,0 +1,18 @@
+
+
+
+var fieldName = 'new- name';
+var fieldPrice = 'price';
+
+const course = {
+  name: 'Javascript',
+  [fieldName]: 'Javascript',
+  [fieldPrice]: 1000
+};
+
+console.log(course)
+
+
+
+
+
